b1cf73e5a733cacfa944b5081204fbbe6bef1337
Ca1igo <127109983+Ca1igo@users.noreply.github.com>
2023-03-06 15:55:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//Tabs
+const tabsBtns = document.querySelectorAll(".tabs__nav button");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+// Функция скрывет табы и убирает active у кнопок
+
+function hideTabs() {
+    tabsItems.forEach(item => item.classList.add("hide"));
+    tabsBtns.forEach(item => item.classList.remove("active"));
+}
+
+// Функция показывает переданный номер таба и делает соответсвующую ему кнопку активной
+
+function showTab(index) {
+    tabsItems[index].classList.remove("hide");
+    tabsBtns[index].classList.add("active");
+}
+
+hideTabs();
+showTab(0);
+
+tabsBtns.forEach((btn, index) => btn.addEventListener("click", () => {
+    hideTabs();
+    showTab(index);
+}));
+
+//Anchors
+
+const anchors = document.querySelectorAll(".header__nav a");
+
+//Плавная прокрутка по якорю
+
+anchors.forEach(anc => {
+    anc.addEventListener("click", function(event) {
+        event.preventDefault();
+    
+        const id = anc.getAttribute("href");
+        const elem = document.querySelector(id);
+    
+        window.scroll ({
+            top: elem.offsetTop -80,
+            behavior: 'smooth'
+        });
+    });
+});            
